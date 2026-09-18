cd93ec5df1223f448df59fe70816256ab836adaf
sigalplay <sigalsplay@gmail.com>
2026-09-18 12:20:51 +0300

    Fix English therapist navigation (1/7)

--- assets/english-route-boot-v93.js
+++ assets/english-route-boot-v93.js
@@ -5,6 +5,15 @@
   document.documentElement.lang = "en";
   document.documentElement.dir = "ltr";
   const path = location.pathname;
+  const routeMap = [
+    ["/en/therapist/", "/therapist/build?view=session"],
+    ["/en/therapist/build/", "/therapist/build?view=session"]
+  ];
+  const mapped = routeMap.find(([englishPath]) => path === englishPath || path === englishPath.slice(0, -1));
+  if (mapped) {
+    location.replace(mapped[1]);
+    return;
+  }
   if (/^\/en\//.test(path)) {
     const appPath = path.slice(3) || "/";
     history.replaceState(history.state, "", `${appPath}${location.search}${location.hash}`);
